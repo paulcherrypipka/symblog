74af029a42c8eb1643dfa9551d891fa93a1b505d
paul cherepenko <cherrypipka@gmail.com>
2017-02-09 23:02:40 +0300

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on January 30, 2017, 10:07 pm.
---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class PageController
+ * @package Blogger\BlogBundle\Controller
+ */
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                //$message = \Swift_Message::newInstance()
+                //    ->setSubject('Contact enquiry from symblog')
+                //    ->setFrom('enquiries@symblog.co.uk')
+                //    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                //    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                //$this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+
+            }
+
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+}
